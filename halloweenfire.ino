c7d2294886d99278d30121b83799c01884382cc8
Dan Esparza <esparza.dan@gmail.com>
2015-10-29 19:40:33 -0400

    Added additional config knobs

--- halloweenfire.ino
+++ halloweenfire.ino
@@ -5,6 +5,10 @@
 
 #define PIN 6
 
+//  The overall fire brightness
+//  (this can affect both color levels and power consumption)
+int brightness = 128;
+
 // Parameter 1 = number of pixels in strip
 // Parameter 2 = Arduino pin number (most are valid)
 // Parameter 3 = pixel type flags, add together as needed:
@@ -12,15 +16,18 @@
 //   NEO_KHZ400  400 KHz (classic 'v1' (not v2) FLORA pixels, WS2811 drivers)
 //   NEO_GRB     Pixels are wired for GRB bitstream (most NeoPixel products)
 //   NEO_RGB     Pixels are wired for RGB bitstream (v1 FLORA pixels, not v2)
-Adafruit_NeoPixel strip = Adafruit_NeoPixel(4, PIN, NEO_GRB + NEO_KHZ800);
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(29, PIN, NEO_GRB + NEO_KHZ800);
 
 // IMPORTANT: To reduce NeoPixel burnout risk, add 1000 uF capacitor across
 // pixel power leads, add 300 - 500 Ohm resistor on first pixel's data input
 // and minimize distance between Arduino and first pixel.  Avoid connecting
 // on a live circuit...if you must, connect GND first.
+// For the ultimate NeoPixel guide, check out:
+// https://learn.adafruit.com/adafruit-neopixel-uberguide/overview
 
 void setup() {
   strip.begin();
+  strip.setBrightness(brightness);
   strip.show(); // Initialize all pixels to 'off'
 }
 
